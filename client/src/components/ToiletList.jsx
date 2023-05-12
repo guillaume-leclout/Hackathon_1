7e03110844ebf982be59df563b53b6ef7537e143
Coopykat <kathycamazon@gmail.com>
2023-05-12 12:26:33 +0200

    footer and font

--- client/src/components/ToiletList.jsx
+++ client/src/components/ToiletList.jsx
@@ -1,12 +1,13 @@
-
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './ToiletList.css';
+import Map from '../components/Map';
+import '../pages/dashboard.css'
+
+import './toiletlist.css';
 
 const ToiletList = () => {
   const [toilets, setToilets] = useState([]);
   const [currLocationJs, setCurrLocationJs] = useState({});
-  const iframeRef = useRef(null);
 
   useEffect(() => {
     const getLocationJs = () => {
@@ -52,38 +53,20 @@ const ToiletList = () => {
     }
   }, [currLocationJs]);
 
-  useEffect(() => {
-    const handleMapDrag = () => {
-      const map = iframeRef.current.contentWindow;
-      map.addEventListener('dragend', () => {
-        const newLatitude = map.lat;
-        const newLongitude = map.lng;
-        setCurrLocationJs({ latitude: newLatitude, longitude: newLongitude });
-      });
-    };
-
-    const iframe = iframeRef.current;
-    if (iframe) {
-      iframe.addEventListener('load', handleMapDrag);
-    }
-
-    return () => {
-      if (iframe) {
-        iframe.removeEventListener('load', handleMapDrag);
-      }
-    };
-  }, []);
   return (
     <div className="ToiletList">
-        <iframe src={`https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d83999.06631218531!2d${currLocationJs.longitude || ""}!3d${currLocationJs.latitude || ""}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1s%20toilettes%20publiques!5e0!3m2!1sfr!2sfr!4v1683809345061!5m2!1sfr!2sfr`} width="800" height="600" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      <Map {...currLocationJs} />
       {toilets &&
         toilets.map((toilet) => (
-          <p key={toilet.id}>{toilet.name} {toilet.city} {toilet.street}</p>
+          <p key={toilet.id}>
+            {toilet.name} {toilet.city} {toilet.street}
+          </p>
+
+
         ))}
+
     </div>
   );
-
 };
-
 
 export default ToiletList;
